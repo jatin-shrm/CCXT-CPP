e312bfaf7a2f4ffd126ed3d9efaf28a8bfe790ac
jatin <jsharma3182002@gmail.com>
2025-08-11 15:10:46 +0530

    added load markets

--- src/base/exchange.hpp
+++ src/base/exchange.hpp
@@ -20,5 +20,5 @@ public:
     virtual nlohmann::json fetch_order_book(const std::string &symbol, const nlohmann::json &params = nlohmann::json::object()) = 0;
     virtual nlohmann::json fetch_orders(const std::string &symbol = "", const std::string &currency = "any", const std::string &kind = "any", const std::string &interval = "raw", const nlohmann::json &extra_params = {}) = 0;
     virtual nlohmann::json create_order(const std::string &symbol, const std::string &type, const std::string &side, double amount, std::optional<double> price = std::nullopt, const nlohmann::json &params = nlohmann::json::object()) = 0;
-    virtual nlohmann::json cancel_order(const std::string &order_id) = 0;
+    virtual nlohmann::json cancel_order(const std::string &id, const std::string &symbol = "", const nlohmann::json &params = nlohmann::json::object()) = 0;
 };
--- src/include/deribit.hpp
+++ src/include/deribit.hpp
@@ -40,7 +40,7 @@ public:
 
     nlohmann::json create_order(const std::string &symbol, const std::string &type, const std::string &side, double amount, std::optional<double> price = std::nullopt, const nlohmann::json &params = nlohmann::json::object()) override;
 
-    nlohmann::json cancel_order(const std::string &order_id) override;
+    nlohmann::json cancel_order(const std::string &id, const std::string &symbol = "", const nlohmann::json &params = nlohmann::json::object()) override;
 
 private:
     bool is_test;
